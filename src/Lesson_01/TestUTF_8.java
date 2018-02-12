d1967854856a4fe0666a2b7c643d15edfc91eabc
h010d <lessonsep@gmail.com>
2018-02-12 03:32:19 +0200

    Test Unicote UTF-8

---
+++ src/Lesson_01/TestUTF_8.java
@@ -0,0 +1,13 @@
+package Lesson_01;
+
+public class TestUTF_8 {
+
+	public static void main(String[] args) {
+		// Тут комментарии на русском
+		int a  = 10;
+		double dbl = 45.3; // комментарии на русском
+				// comment English
+
+	}
+
+}
