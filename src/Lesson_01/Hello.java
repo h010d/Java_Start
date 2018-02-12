29913a986743b00d633da63d9898cdf0e50685b9
h010d <lessonsep@gmail.com>
2018-02-12 03:23:43 +0200

    Исправил кодировку

--- src/Lesson_01/Hello.java
+++ src/Lesson_01/Hello.java
@@ -1,11 +1,10 @@
 package Lesson_01;
 
 public class Hello {
-	public static  int площадь = 10;
-// ============
+	public static  int площадь= 10;
 	public static void main(String[] args) {
 		System.out.println("Hello Java World!");
-		System.out.println("ѕлощадь = " + площадь);
+		System.out.println("площадь = " + площадь);
 
 	}
 
