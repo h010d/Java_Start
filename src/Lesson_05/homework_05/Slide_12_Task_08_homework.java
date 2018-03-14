f7249d7090a1b6fda098fd61ce47d71839f6d160
h010d <lessonsep@gmail.com>
2018-03-15 01:11:09 +0200

    create Class

---
+++ src/Lesson_05/homework_05/Slide_12_Task_08_homework.java
@@ -0,0 +1,53 @@
+package Lesson_05.homework_05;
+
+
+public class Slide_12_Task_08_homework {
+    public static void main(String[] args) {
+
+        //TODO: 8. Создать метод, возвращающий true, если заданное число находится "повсюду" в целочисленном массиве. Под "повсюду"
+//        подразумевается, что при рассмотрении любой пары рядом
+//        стоящих элементов массива, одним из элементов будет искомое
+//        число, т.е.
+//                isEverywhere({1, 2, 1, 3}, 1) → true
+//        isEverywhere({1, 2, 1, 3}, 2) → false
+//        isEverywhere({1, 2, 1, 3, 4}, 1) → false
+        int Arr[] = {0, 3, 0, 3, 5, 3, 4, 3, 7, 3, 9, 3, 0};
+        boolean bool = isEverywhere(Arr, 3);
+    }
+    public static boolean isEverywhere(int[] Arr, int Dgt) {
+        boolean bl = false;
+        int a = Dgt;
+        int arr[] = Arr;
+        int i = 0;
+        int index = 0;
+        while (i < arr.length) {
+            index = i % 2;
+            if((arr[i] - a) == 0){
+                switch (index) {
+                    case 0:
+                        if (a == arr[i]) {
+                            bl = true;
+                            i += 2;
+                        }
+                        break;
+                    case 1:
+                        if (a == arr[i]) {
+                            bl = true;
+                            i += 2;
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }else if( (arr[i + 1] - a) == 0){
+                i++;
+            }else{
+                bl = false;
+                break;
+            }
+        }
+        System.out.println("bl = " + bl);
+        return bl;
+    }
+}
---
+++ src/Lesson_05/homework_05/Slide_12_Task_09_homework.java
@@ -0,0 +1,21 @@
+package Lesson_05.homework_05;
+
+public class Slide_12_Task_09_homework {
+    public static void main(String[] args) {
+//        // TODO: 9. Создать метод, проверяющий, может ли массив быть
+//        "сбалансированным", т.е. разделённым на две части в каком-то
+//        месте, таким образом, чтобы сумма элементов одной части
+//        равнялась сумме элементов второй. Т.е.
+//        { 1, 1, 1, 2, 1 }; // true
+//        { 1, 2, 3, 1, 0, 1, 3 }; // false
+//        { 1, 1, 1, 1, 4 }; // true
+int arr[] = {1, 2, 3, 6, 9, 1, 2};
+        for (int a :arr) {
+            System.out.print( " a = " + a );
+        }
+        boolean search = true;
+        while (search){
+
+        }
+    }
+}
