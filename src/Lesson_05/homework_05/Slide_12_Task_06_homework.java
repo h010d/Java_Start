d8aa861fab542b8ad1e6a76920072e7b7785e568
h010d <lessonsep@gmail.com>
2018-03-15 09:09:35 +0200

    create Class

--- src/Lesson_05/homework_05/Slide_12_Task_06_homework.java
+++ src/Lesson_05/homework_05/Slide_12_Task_06_homework.java
@@ -1,9 +1,17 @@
 package Lesson_05.homework_05;
 
+
+
 import java.util.Scanner;
 
 public class Slide_12_Task_06_homework {
     public static void main(String[] args) {
+//        TODO: 6. Путешественник проходит каждый день несколько километров.
+//        Создать метод, выводящий на экран его путь с начала
+//        путешествия(в виде "День №1 : 10км; День №2 : 7км; День №3 :
+//                13км; "). Метод должен работать для любого количества дней
+//        путешествия.
+        // http://cloud-notes.blogspot.com/2011/07/java.html
         Scanner scanner = new Scanner(System.in);
         int testInt = 0;
         String srt = "";
