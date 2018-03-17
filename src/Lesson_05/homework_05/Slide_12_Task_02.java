c9d6cfba4845ae1f9400d11176b4a61973c2dce2
h010d <lessonsep@gmail.com>
2018-03-17 04:11:57 +0200

    Solution Slide_12_Task_02 2. Создать метод проверяющий, что у переданного числа первая цифра равна последней

---
+++ src/Lesson_05/homework_05/Slide_12_Task_02.java
@@ -0,0 +1,29 @@
+package Lesson_05.homework_05;
+
+public class Slide_12_Task_02 {
+
+	public static void main(String[] args) {
+//		Создать метод проверяющий, что у переданного числа первая
+//		цифра равна последней
+		int nm = 5632565;
+		equals(nm);
+
+	}
+	public static boolean equals(int Num) {
+		boolean bl = false;
+		int lastnumb = Num % 10;
+		int firstnumb = Num;
+		while(firstnumb > 10){
+			firstnumb /= 10;
+
+		}
+		if (lastnumb == firstnumb){
+			bl = true;
+		}
+		System.out.println(firstnumb);
+		System.out.println(lastnumb);
+		System.out.println(bl);
+		return bl;
+	}
+
+}
