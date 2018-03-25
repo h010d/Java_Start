b67854032f18308d270c7470d5d3de1ec31378c5
h010d <lessonsep@gmail.com>
2018-03-25 12:29:14 +0300

    занятие дома

--- src/Lesson_06/Shape_test.java
+++ src/Lesson_06/Shape_test.java
@@ -10,7 +10,7 @@ class PointTest {
     private int coordX;
     private int coordY;
     public PointTest(){
-        System.out.println("I'm Point");
+        System.out.println("I'm class Point");
     }
 }
 
@@ -18,13 +18,18 @@ class LengthTest {
     PointTest pointTest1;
     PointTest pointTest2;
     public LengthTest(){
-        System.out.println("I'm LengthTest");
+this.pointTest1 = new PointTest();
+this.pointTest2 = new PointTest();
+        System.out.println("I'm class LengthTest");
     }
 }
 
 class TriangleTest {
     private LengthTest lengthTest1, lengthTest2, lengthTest3;
     public TriangleTest(){
-        System.out.println("I'm TriangleTest");
+        this.lengthTest1 = new LengthTest();
+        this.lengthTest2 = new LengthTest();
+        this.lengthTest3 = new LengthTest();
+        System.out.println("I'm class TriangleTest");
     }
 }
