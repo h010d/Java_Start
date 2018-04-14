87eb60fceb6df8a213c5c79f4e15f3c2d0f4a454
h010d <lessonsep@gmail.com>
2018-04-14 12:23:26 +0300

    homework ready

--- src/Lesson_08/HomeworkConsoleAWTGridLayout.java
+++ src/Lesson_08/HomeworkConsoleAWTGridLayout.java
@@ -10,24 +10,16 @@ public class HomeworkConsoleAWTGridLayout {
     Color color;
     int column;
     int row;
-    int width;
-    int height;
-//    int heightField;
-//    int widthField;
     private Button button;
-    static int coordx = 0;
-    static int coordy = 0;
 
     public HomeworkConsoleAWTGridLayout() {
         f = new Frame("Hello I'm There!");
         col = new int[3];
-        width = 45;
-        height = 31;
+
     }
     public void launchFrame(int amount) {
         sqrtSize(amount);
         f.setLayout(new GridLayout(column, row));
-//        System.out.println("column = " + column + " row = " + row);
         f.setBackground(Color.blue);
         for (int i = 0; i < row; i++) {
             for (int k = 0; k < column && amount > 0; k++, amount--) {
@@ -37,12 +29,8 @@ public class HomeworkConsoleAWTGridLayout {
                 color = new Color(col[0], col[1], col[2]);
                 button = new Button();
                 button.setBackground(color);
-                button.setBounds(coordx, coordy, width, height);
                 f.add(button);
-                coordx += width;
             }
-            coordx = 0;
-            coordy += height;
         }
         f.pack();
         f.setVisible(true);
@@ -63,7 +51,7 @@ public class HomeworkConsoleAWTGridLayout {
         System.out.println("Сколько квадратов нужно вывести на экране? ");
         do {
             amount = scanner.nextInt();
-        } while ((amount <= 5) || (amount > 999));
+        } while ((amount <= 0) || (amount > 999));
         HomeworkConsoleAWTGridLayout guiWindow = new HomeworkConsoleAWTGridLayout();
         guiWindow.launchFrame(amount);
     }
